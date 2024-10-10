3eb7495e5328ab2fc8636a2dc0ddc9a67c7661b6
assoly-lab <81777632+assoly-lab@users.noreply.github.com>
2024-10-10 23:05:20 +0100

    final commit for real for resl 2

--- components/FaqAccordion.tsx
+++ components/FaqAccordion.tsx
@@ -35,9 +35,9 @@ const FaqAccordion = () => {
   };
 
   return (
-    <div className="md:w-[70%] flex flex-col gap-2">
+    <div className="w-[90%] md:w-[70%] flex flex-col gap-2">
       {faqData.map((faq, index) => (
-        <div key={index} className="w-full border border-gray-200 py-4 px-4 rounded-md">
+        <div key={index} className="border border-gray-200 py-4 px-4 rounded-md">
           <div
             className="cursor-pointer text-base md:text-lg font-semibold flex justify-between items-center  "
             onClick={() => toggleAccordion(index)}
