4e0792fca0d44ee26fe1fe0b8d4bc11ba8ab24d7
assoly-lab <81777632+assoly-lab@users.noreply.github.com>
2024-10-11 19:00:24 +0100

    further fixes for the product slider

--- components/ProductSlider.tsx
+++ components/ProductSlider.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useState } from 'react';
-import Slider from "react-slick";
 import { Img, Product } from '@/utils/Types';
 import Image from 'next/image';
 
@@ -11,33 +10,11 @@ export default function ProductSlider({product}:{product:Product}){
   const [mainImage,setMainImage] = useState<string>(product.main_image)
   const images:Img[] = [{image:product.main_image},...product.images]
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows:false,
-    fade:true,
-    appendDots: (dots:React.ReactNode[]) => (
-        <ul style={{gap:"30px",display:'block !important',margin:"0 auto !important",width:"90% !important",overflowX:"auto"}} className="">
-            {images.map((image:Img, index:number) => (
-                <li style={{width:"100px !important",cursor:"pointer",height:"100px !important",display:"inline-block"}} key={index} className=" cursor-pointer border border-gray-100" onClick={()=>setMainImage((prev:string) => prev != image.image ? image.image: prev)}>
-                    <Image className='' src={image.image} width={100} height={100} alt={product.name}/>
-                </li>
-            ))}
-        </ul>
-    ),
-    // customPaging: (i: number) => (
-    //     <button className="before:!text-base before:!text-red-500 md:before:!text-base "></button> // Default gray color for inactive dots
-    // ),
-};
+ 
 
   return (
     <div className="relative flex flex-col items-center">
-      {/* Main Image Slider */}
       <div style={{zIndex:'0'}} className="mb-8">
-      {/* <Slider {...settings}> */}
                     <div className="w-screen md:w-auto">
                         <div className="pb-4 flex items-center justify-center overflow-hidden z-0">
                             <Image
@@ -57,7 +34,6 @@ export default function ProductSlider({product}:{product:Product}){
             ))}
         </ul>
                     </div>
-            {/* </Slider> */}
       </div>
     </div>
   );
