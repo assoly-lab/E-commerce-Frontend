e711e2998ec4f0df9646e0099c18d8f66d993b11
assoly-lab <81777632+assoly-lab@users.noreply.github.com>
2024-10-12 17:47:03 +0100

    fixed the best offers section

--- components/BestOffers.tsx
+++ components/BestOffers.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Countdown from "./Countdown";
 
 
@@ -9,11 +10,13 @@ export default function BestOffers(){
             <h3 className="md:text-5xl text-3xl text-black">Best Offers - Up to <span className="text-[#E73F10]">50%</span></h3>
             <p className="">Explore our latest New Arrivals & unlock discounts of up to 50% off!</p>
             <Countdown targetDate={targetDate}  />
-            <button
-                className="relative z-10 md:mt-20 px-6 py-2 bg-[#E73F10] text-white text-lg rounded overflow-hidden transition duration-300 hover:text-white before:absolute before:inset-0 before:bg-amber-400 before:w-0 before:h-full before:transition-all before:duration-300 hover:before:w-full before:z-0"
-                >
-                <span className="relative z-20">View All</span>
-            </button>
+            <Link className="mx-auto" href={'/products'}>
+                <button
+                    className="relative z-10 md:mt-20 px-6 py-2 bg-[#E73F10] text-white text-lg rounded overflow-hidden transition duration-300 hover:text-white before:absolute before:inset-0 before:bg-amber-400 before:w-0 before:h-full before:transition-all before:duration-300 hover:before:w-full before:z-0"
+                    >
+                    <span className="relative z-20">View All</span>
+                </button>
+            </Link>
         </div>
     )
 }
